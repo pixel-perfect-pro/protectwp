9f4e3869b206f50b38fd5dc4345d00194f6b165b
Paweł Lewczuk <tech@pixel-perfect.pro>
2024-04-16 14:41:32 +0200

    Add new Google Tag Manager codes

--- wp-content/themes/respo/functions.php
+++ wp-content/themes/respo/functions.php
@@ -177,13 +177,13 @@ add_action( 'wp_head', 'pixelperfect_wp_head', 1 );
 
 function pixelperfect_wp_head() { ?>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WQ4FMNK');</script>
-<!-- End Google Tag Manager -->
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P499JRGD');</script>
+	<!-- End Google Tag Manager -->
 
 <?php }
 
@@ -193,7 +193,7 @@ add_action( 'wp_body_open', 'pixelperfect_wp_body_open' );
 function pixelperfect_wp_body_open() { ?>
 
 	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQ4FMNK"
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P499JRGD"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<!-- End Google Tag Manager (noscript) -->
 
